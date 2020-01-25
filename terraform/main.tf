c5a855470f7856192654680acff489ff67dc7ee9
kagami ryohei <a7112069@gmail.com>
2020-01-25 21:48:29 +0900

    delete some .tf file for reconstruction

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region  = "ap-northeast-1"
-  version = "~> 2.8"
-}
-
 terraform {
+  required_version = "= 0.12.20"
   backend "s3" {
     bucket = "c0fee-tfstate"
     key    = "terraform/.terraform/terraform.tfstate"
     region = "ap-northeast-1"
   }
+}
+
+provider "aws" {
+  version = "= 2.44.0"
 }
 
 resource "aws_s3_bucket" "tfstate" {
@@ -26,56 +26,3 @@ resource "aws_s3_bucket" "tfstate" {
     }
   }
 }
-
-# resource "aws_s3_bucket" "cloudwatch_logs" {
-#   bucket = "cloudwatch-logs-C0FEE"
-
-#   versioning {
-#     enabled = true
-#   }
-
-#   lifecycle_rule {
-#     enabled = true
-
-#     expiration {
-#       days = "180"
-#     }
-#   }
-# }
-
-# data "aws_iam_policy_document" "kinesis_data_firehose" {
-#   statement {
-#     effect = "Allow"
-
-#     actions = [
-#       "s3:AbortMultipartUpload",
-#       "s3:GetBucketLocation",
-#       "s3:GetObject",
-#       "s3:ListBucket",
-#       "s3:ListBucketMultipartUploads",
-#       "s3:PutObject",
-#     ]
-
-#     resources = [
-#       "arn:aws:s3:::${aws_s3_bucket.cloudwatch-logs.id}",
-#       "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
-#     ]
-#   }
-# }
-
-# module "kinesis_data_firehose_role" {
-#   source     = "./iam_role"
-#   name       = "kinesis-data-firehose"
-#   identifier = "firehose.amazonaws.com"
-#   policy     = data.aws_iam_policy_document.kinesis_data_firehose.json
-# }
-
-# resource "aws_kinesis_firehose_delivery_stream" "c0fee" {
-#   name        = "c0fee"
-#   destination = "s3"
-
-#   s3_configuration {
-#     role_arn   = module.kinesis_data_firehose_role.iam_role_arn
-#     bucket_arn = aws_s3_bucket.cloudwatch_logs.arn
-#   }
-# }
