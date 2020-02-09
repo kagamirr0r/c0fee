1bda16dc42a39e1811410231976394dcb1cd0011
kagami ryohei <a7112069@gmail.com>
2020-02-09 23:36:33 +0900

    fix Dockerfile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :twitter, Rails.application.credentials.dig(:twitter,:api_key),Rails.application.credentials.dig(:twitter,:api_secret_key)
+  config.omniauth :twitter, Rails.application.credentials.twitter[:api_key],Rails.application.credentials.twitter[:api_secret_key]
 end
