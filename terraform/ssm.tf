032b46e079a90469a0eb8efd1263044486f33dc2
kagami ryohei <a7112069@gmail.com>
2020-03-15 15:51:22 +0900

    fix terraform/ssm.tf to set environment variable in containers

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -9,3 +9,91 @@ resource "aws_ssm_parameter" "rails_master_key" {
   }
 }
 
+resource "aws_ssm_parameter" "postgres_user" {
+  name        = "/rails/postgres_user"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "POSTGRES_USER"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "postgres_password" {
+  name        = "/rails/postgres_password"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "POSTGRES_PASSWORD"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/rails/db_username"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "DB_USERNAME"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "/rails/db_password"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "DB_PASSWORD"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "rds_host" {
+  name        = "/rails/rds_host"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "RDS_HOST"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "rds_database" {
+  name        = "/rails/rds_database"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "RDS_DATABASE"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "rds_username" {
+  name        = "/rails/rds_username"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "RDS_USERNAME"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "rds_password" {
+  name        = "/rails/rds_password"
+  value       = "hoge"
+  type        = "SecureString"
+  description = "RDS_PASSWORD"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
