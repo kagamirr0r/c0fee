b513b63fcacb4b51726c8eb71f89075f8c328f2d
kagami ryohei <a7112069@gmail.com>
2020-05-27 23:22:55 +0900

    fix some spec files

--- spec/features/bean_like_spec.rb
+++ spec/features/bean_like_spec.rb
@@ -8,12 +8,11 @@ RSpec.feature 'BeanLikes', type: :feature do
 
   scenario 'bean_like_button' do
 		visit beans_path
-		expect(page).to have_content impression.bean.country
     click_button I18n.t('bean_likes.like_bean_button.like_bean')
-    expect(page).to have_content t('.cancel_like_bean')
+    expect(page).to have_content I18n.t('bean_likes.like_bean_button.cancel_like_bean')
 
     visit beans_path
-    click_button I18n.t('bean_likes.like_bean_button.like_bean')
-    expect(page).to have_content t('.like_bean')
+    click_button I18n.t('bean_likes.like_bean_button.cancel_like_bean')
+    expect(page).to have_content I18n.t('bean_likes.like_bean_button.like_bean')
   end
 end
