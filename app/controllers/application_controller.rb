cc959ad3cedf49953e06bb464ecec2245bb02fea
kagami ryohei <a7112069@gmail.com>
2020-05-27 13:07:36 +0900

    run rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
       my_page_path
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
 
   private
   def sign_in_required
-			redirect_to new_user_session_url unless user_signed_in?
-	end
+      redirect_to new_user_session_url unless user_signed_in?
+  end
 
   def set_locale
     I18n.locale = locale
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
     @locale ||= params[:locale] || I18n.default_locale
   end
 
-  def default_url_options(options={})
+  def default_url_options(options = {})
     options.merge(locale: locale)
   end
 
