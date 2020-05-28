68a2b4bd9f82d96d0995c7c4023af966edb3f329
kagami ryohei <a7112069@gmail.com>
2020-05-28 14:07:24 +0900

    test

--- spec/features/bean_like_spec.rb
+++ spec/features/bean_like_spec.rb
@@ -9,10 +9,10 @@ RSpec.feature 'BeanLikes', type: :feature do
   scenario 'bean_like_button' do
     visit beans_path
     click_button I18n.t('bean_likes.like_bean_button.like_bean')
-    expect(page).to have_content I18n.t('bean_likes.like_bean_button.cancel_like_bean')
+    expect(page).to has_button? I18n.t('bean_likes.like_bean_button.cancel_like_bean')
 
     visit beans_path
     click_button I18n.t('bean_likes.like_bean_button.cancel_like_bean')
-    expect(page).to have_content I18n.t('bean_likes.like_bean_button.like_bean')
+    expect(page).to has_button? I18n.t('bean_likes.like_bean_button.like_bean')
   end
 end
