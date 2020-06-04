bbac5edd5663da8d127e538b465b0641329d2a4f
kagami ryohei <a7112069@gmail.com>
2020-06-04 16:15:57 +0900

    fix like button

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -5,9 +5,9 @@ class BeansController < ApplicationController
     @bean_search_params = bean_search_params
 
     @beans = if @bean_search_params.present?
-               Bean.with_translations(I18n.locale).search_bean(@bean_search_params).page.(params[:page])
+               Bean.with_translations(I18n.locale).search_bean(@bean_search_params).page(params[:page])
              else
-               Bean.all.page(params[:page])
+               Bean.page(params[:page])
              end
   end
 
