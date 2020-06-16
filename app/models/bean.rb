e38d9fa36b5ddce118bc98215651d33519fcc0e6
kagami ryohei <a7112069@gmail.com>
2020-06-16 12:29:58 +0900

    download flag images

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -3,8 +3,14 @@ class Bean < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
   validates :country, presence: true
-  translates :country, :area, :variety, :farm
 
+  translates :area, :variety, :farm
+
+  enum country: { Yemen: 0, Ethiopia: 1, Tanzania: 2, Kenya: 3, Rwanda: 4, Burundi: 5, India: 6,
+                  Indonesia: 7, PNG: 8, Mexico: 9, Guatemala: 10, Costa_Rica: 11, El_Salvador: 12,
+                  Nicaragua: 13, Honduras: 14, Panama: 15, Dominica: 16, Haiti: 17, Jamaica: 18, Brazil: 19,
+                  Colombia: 20, Ecuador: 21, Peru: 22, Bolivia: 23, Hawaii: 24, Cuba: 25, Nepal: 26, China: 27,
+                  Vietnam: 28, East_Timor: 29, Myanmar: 30 }
   enum process: { washed: 0, natural: 1, pulped_natural: 2, white_honey: 3, yellow_honey: 4, red_honey: 5, black_honey: 6, sumatera: 7 }
   enum roast: { light: 0, cinnamon: 1, medium: 2, high: 3, city: 4, fullcity: 5, french: 6, italian: 7 }
 
@@ -28,7 +34,7 @@ class Bean < ApplicationRecord
       .merge(Impression.search_impression(bean_search_params))
   end
 
-  scope :country_search, ->(country) { where('bean_translations.country LIKE ?', "%#{country}%") if country.present? }
+  scope :country_search, ->(country) { where(country: country) if country.present? }
   scope :roast_search, ->(roast) { where(roast: roast) if roast.present? }
 
   mount_uploader :bean_image, ImageUploader
