2b1c174531feb4bd055cfb19e5806c4c04575889
kagami ryohei <a7112069@gmail.com>
2020-06-18 13:59:52 +0900

    fix tests

--- spec/features/bean_spec.rb
+++ spec/features/bean_spec.rb
@@ -27,7 +27,7 @@ RSpec.feature 'Beans', type: :feature do
     select impression.i_bitter, from: 'bean_impression_attributes_i_bitter'
     fill_in 'bean_impression_attributes_i_comment', with: impression.i_comment
 		click_button I18n.t('beans.form.register')
-		expect(page).to have_content I18n.t('beans.flash.registered_bean'), match: :first
+		expect(find('.teal').text).to eq I18n.t('beans.flash.registered_bean')
 
 
     # show
