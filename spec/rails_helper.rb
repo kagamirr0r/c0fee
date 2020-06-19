34466ac9b00ee345d3e9ce683901e45825b02edf
kagami ryohei <a7112069@gmail.com>
2020-06-19 15:37:25 +0900

    fix spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,10 +39,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, options: {
       browser: :remote,
       url: ENV.fetch('SELENIUM_DRIVER_URL'),
