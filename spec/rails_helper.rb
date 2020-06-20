b990217b72e2c3ee086015c56d800edc1ea2015e
kagami ryohei <a7112069@gmail.com>
2020-06-21 00:38:00 +0900

    fix tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,8 +29,6 @@ Capybara.register_driver :remote_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
 end
 
-Capybara.javascript_driver = :selenium_chrome_headless
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
