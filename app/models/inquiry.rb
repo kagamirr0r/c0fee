3d3543fce369f91f49daf6a72d06d32ceee764f0
kagami ryohei <a7112069@gmail.com>
2020-06-25 20:12:06 +0900

    fix validate message

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,5 @@
 class Inquiry < ApplicationRecord
   validates :name, presence: { message: 'お名前をご入力ください'}
   validates :email, presence: { message: 'メールアドレスをご入力ください'}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'メールアドレスの形式をご確認ください'}
-  validates :message, presence: { message: 'お問い合わせ内容をご入力ください'}, length: { in: 1..300, message: 'お問い合わせ内容は１文字以上３００文字以内でご入力ください' }
+  validates :message, presence: { message: 'お問い合わせ内容をご入力ください'}, length: { in: 10..300, message: 'お問い合わせ内容は１0から300文字以内でご入力ください' }
 end
