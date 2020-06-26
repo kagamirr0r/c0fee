09b4c8ebb57b169b8b0bc6a26c0a20500eb90d12
kagami ryohei <a7112069@gmail.com>
2020-06-26 23:13:06 +0900

    fix spec

--- spec/factories/inquiry.rb
+++ spec/factories/inquiry.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :inquiry, class: Inquiry do
     name { Faker::Name.name }
-    message { Faker::String.random(length: 10..300) }
+    message { "aaaaaaaaaaaaaaaaaaaaaaaa" }
     email { Faker::Internet.free_email }
   end
 end
