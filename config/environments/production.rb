77980ec89a6dbde8a916b7988c3a1d1d44158cf0
kagami ryohei <a7112069@gmail.com>
2020-08-05 11:22:02 +0900

    fix config logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,9 +95,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    #logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = Logger.new(STDOUT)
   end
 
   # Do not dump schema after migrations.
