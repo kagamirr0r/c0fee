e88d5fa9898e68320546be5259f0a79ee2c9c61a
kagami ryohei <a7112069@gmail.com>
2020-08-05 19:36:40 +0900

    fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
-
+	config.action_mailer.default_url_options = { host: 'c0fee.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -74,7 +74,8 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.perform_caching = false
+	config.action_mailer.perform_caching = false
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
